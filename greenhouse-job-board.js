4e4be33005b3bcf4e9f28fc2eda2a3596a59a416
Megggle <megansauer@outlook.com>
2025-04-14 09:59:51 -0400

    Update greenhouse-job-board.js

--- greenhouse-job-board.js
+++ greenhouse-job-board.js
@@ -1,6 +1,14 @@
 class GreenhouseJobBoard extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = "<h2>Greenhouse Widget Loaded!</h2>";
+    const script = document.createElement('script');
+    script.src = 'https://boards.greenhouse.io/embed/job_board/js?for=nuclearpromisex';
+    script.onload = () => {
+      const container = document.createElement('div');
+      container.id = 'grnhse_app';
+      this.appendChild(container);
+    };
+    document.head.appendChild(script);
   }
 }
+
 customElements.define('greenhouse-job-board', GreenhouseJobBoard);
